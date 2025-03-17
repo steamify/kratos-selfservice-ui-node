bd1a1412feb2bfa2ce0699871df92fc17ba6ccec
maxim.shnyagin <maxim.shnyagin@gmail.com>
2025-03-17 11:47:52 +0300

    log

--- src/pkg/index.ts
+++ src/pkg/index.ts
@@ -12,6 +12,7 @@ import { ButtonLink, Divider, MenuLink, Typography } from "@ory/elements-markup"
 import { AxiosError } from "axios"
 import { NextFunction, Response } from "express"
 import { UnknownObject } from "express-handlebars/types"
+import { logger } from "./logger"
 
 export * from "./logger"
 export * from "./middleware"
@@ -23,8 +24,7 @@ export const getUrlForFlow = (
   flow: string,
   query?: URLSearchParams,
 ) =>
-  `${removeTrailingSlash(base)}/self-service/${flow}/browser${
-    query ? `?${query.toString()}` : ""
+  `${removeTrailingSlash(base)}/self-service/${flow}/browser${query ? `?${query.toString()}` : ""
   }`
 
 export const defaultConfig: RouteOptionsCreator = () => {
@@ -35,8 +35,8 @@ export const defaultConfig: RouteOptionsCreator = () => {
     faviconType: "image/png",
     isOAuthConsentRouteEnabled: () =>
       (process.env.HYDRA_ADMIN_URL || process.env.ORY_SDK_URL) &&
-      process.env.CSRF_COOKIE_SECRET &&
-      process.env.CSRF_COOKIE_NAME
+        process.env.CSRF_COOKIE_SECRET &&
+        process.env.CSRF_COOKIE_NAME
         ? true
         : false,
     shouldSkipConsent: (challenge) => {
@@ -83,42 +83,44 @@ const isErrorAuthenticatorAssuranceLevel = (
 // or 403 error code.
 export const redirectOnSoftError =
   (res: Response, next: NextFunction, redirectTo: string) =>
-  (err: AxiosError) => {
-    if (!err.response) {
-      next(err)
-      return
-    }
+    (err: AxiosError) => {
+      logger.error("redirectOnSoftError error", { error: err })
 
-    if (err.response.status === 401) {
-      // redirect to login
-      const query = new URLSearchParams()
-      query.set("return_to", redirectTo)
-      res.redirect(getUrlForFlow(apiBaseUrl, "login", query))
-      return
-    }
-    if (
-      err.response.status === 404 ||
-      err.response.status === 410 ||
-      err.response.status === 403
-    ) {
-      // in some cases Kratos will require us to redirect to a different page when the session_aal2_required
-      // for example, when recovery redirects us to settings
-      // but settings requires us to redirect to login?aal=aal2
-      const authenticatorAssuranceLevelError = err.response.data as unknown
-      if (
-        isErrorAuthenticatorAssuranceLevel(authenticatorAssuranceLevelError)
-      ) {
-        res.redirect(
-          authenticatorAssuranceLevelError.redirect_browser_to || redirectTo,
-        )
+      if (!err.response) {
+        next(err)
         return
       }
-      res.redirect(`${redirectTo}`)
-      return
-    }
 
-    next(err)
-  }
+      if (err.response.status === 401) {
+        // redirect to login
+        const query = new URLSearchParams()
+        query.set("return_to", redirectTo)
+        res.redirect(getUrlForFlow(apiBaseUrl, "login", query))
+        return
+      }
+      if (
+        err.response.status === 404 ||
+        err.response.status === 410 ||
+        err.response.status === 403
+      ) {
+        // in some cases Kratos will require us to redirect to a different page when the session_aal2_required
+        // for example, when recovery redirects us to settings
+        // but settings requires us to redirect to login?aal=aal2
+        const authenticatorAssuranceLevelError = err.response.data as unknown
+        if (
+          isErrorAuthenticatorAssuranceLevel(authenticatorAssuranceLevelError)
+        ) {
+          res.redirect(
+            authenticatorAssuranceLevelError.redirect_browser_to || redirectTo,
+          )
+          return
+        }
+        res.redirect(`${redirectTo}`)
+        return
+      }
+
+      next(err)
+    }
 
 export const handlebarsHelpers: UnknownObject = {
   jsonPretty: (context: any) => JSON.stringify(context, null, 2),
